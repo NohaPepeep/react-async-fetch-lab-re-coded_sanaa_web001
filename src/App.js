4e2293aa78134ec6742c72c2b427cb6a16cb3ea3
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-22 08:02:15 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ class App extends Component {
   state = {
     peopleInSpace: []
   }
-  componentDidMount = () => {
-     fetch("http://api.open-notify.org/astros.json")
-    .then(response => response.json())
-    .then(data => console.log(data);
+  
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          peopleInSpace: data.people
+        })
+      })
   }
   render() {
     return (
